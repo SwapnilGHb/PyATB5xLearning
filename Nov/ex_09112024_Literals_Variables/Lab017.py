43b676ccc81e5e46fc664fae69e3aa5e87cef3c0
SwapnilGHb <swapnilbahe@gmail.com>
2024-11-14 21:29:11 +0530

    09-Nov-24 commit and push

---
+++ Nov/ex_09112024_Literals_Variables/Lab017.py
@@ -0,0 +1,21 @@
+"""name = "Pramod"
+name2 = "Pramod"
+print(type(name)) #<class 'str'>
+print(type(name2)) #<class 'str'>
+print(name2)
+"""
+
+
+"""
+This is Multiple Line Comment
+Here the code you write will not be executed
+Why this is used?
+
+Notes or Important Information if you want to give
+
+"""
+
+#This is single comment
+
+
+print("Hi")
---
+++ Nov/ex_09112024_Literals_Variables/Lab020_Builtin_Functions.py
@@ -0,0 +1,9 @@
+# Built in Functions
+#Predefined gived by the python guys to use it
+
+#max()
+#min()
+#print()
+print(pow(2,3))
+b = abs(-10) #Returns the absolute value of the argument
+print(b)
