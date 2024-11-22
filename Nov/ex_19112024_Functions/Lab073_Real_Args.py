134358dfc09e72248c6bbb8d9e8aefb22f826d68
SwapnilGHb <swapnilbahe@gmail.com>
2024-11-22 11:37:47 +0530

    19-Nov-24 commit and push

--- Nov/ex_19112024_Functions/Lab073_Real_Args.py
+++ Nov/ex_19112024_Functions/Lab073_Real_Args.py
@@ -6,7 +6,7 @@ def make_pizza(*toppings):
     for i in toppings:
         print(i)
 
-pramod = make_pizza("tomato","olive")
+pramod = make_pizza("tomato","olive","corn")
 jayati = make_pizza("pineapple","olives","corn","paneer")
 vinay = make_pizza("tomato")
 
