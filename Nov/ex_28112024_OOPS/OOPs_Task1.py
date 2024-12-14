979b93bac32793c437b5a15fef62d04ff888daa7
SwapnilGHb <swapnilbahe@gmail.com>
2024-12-14 20:13:08 +0530

    28-Nov-24 commit and push

--- Nov/ex_28112024_OOPS/OOPs_Task1.py
+++ Nov/ex_28112024_OOPS/OOPs_Task1.py
@@ -1,4 +1,4 @@
-#Task 1 - Create a Class PyATB , 5 Attributes, 3 Functions/Methods
+#Create a Class PyATB , 5 Attributes, 3 Functions/Methods
 #Use PC - to set the value of the attributes
 #create a Print student information method/function
 #3 studetns objects
